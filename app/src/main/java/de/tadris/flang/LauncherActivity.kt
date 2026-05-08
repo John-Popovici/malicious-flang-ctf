bf3e69e609ac22526820b9c8429a03e4bd31e809
John P. <john.pop@hotmail.com>
2026-05-08 03:47:50 +0200

    Implemented example flag

--- app/src/main/java/de/tadris/flang/LauncherActivity.kt
+++ app/src/main/java/de/tadris/flang/LauncherActivity.kt
@@ -3,6 +3,7 @@ package de.tadris.flang
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import de.tadris.flang.ui.activity.*
 
 class LauncherActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class LauncherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_launcher)
 
         startActivity(Intent(this, MainActivity::class.java))
+        Toast.makeText(this, "FLAG{welcome-to-the-ctf}", Toast.LENGTH_LONG).show()
         finish()
     }
 }
